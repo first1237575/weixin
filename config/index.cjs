e51d0c5e32b2864a8af901137f50c99ba6e04b0f
first1237575 <113652174+first1237575@users.noreply.github.com>
2023-06-01 16:54:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '4e09847354a39dd4dcb6a5ba43ddcea0',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '湖北',
+  CITY: '武汉',
 
   USERS: [
     {
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCP5y6DSfvLtkLnkTIrc8mS0ao_8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'dEUd3UrE-WZ_Ti9HnKMel48SDAjijOoMQevkGNThXC4',
+      useTemplateId: '8zcVS6eVtVrzYEt2GJbTFqKHjz2Uhvbd86DwAxwxMxs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
