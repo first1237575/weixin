e593f29f188fc59cb19ad9eaf3476ff95da39854
first1237575 <113652174+first1237575@users.noreply.github.com>
2023-06-02 21:55:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -32,7 +32,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oCP5y6DSfvLtkLnkTIrc8mS0ao_8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '2RAoA3MfwtUjvF8l2T9KXo2xnyNb7HILE0owGzuQeEo',
+      useTemplateId: 'o--MRx5OlQaXybrZwCXniNGPmFTbzCixaarRDfH2A9E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
