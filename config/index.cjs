39428f282e745349f08afa68681eb453a25d0c9d
first1237575 <113652174+first1237575@users.noreply.github.com>
2023-06-12 23:33:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -38,7 +38,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '小童同学生日', year: '2003', date: '06-05',
         },
         {
           type: '节日', name: '小童同学生日', year: '2003', date: '06-05',
